b8c15dda719f5b0935a56d3694a098ac27fd8a80
Enoch <enochmokny@gmail.com>
2023-08-12 17:10:17 +0800

    initial commit

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+num_clients=5
+
+# Federated Training
+echo "Starting server"
+python server.py &
+sleep 3  # Sleep for 3s to give the server enough time to start
+
+sequence=$(seq 1 $num_clients)
+
+for i in $sequence; do
+    echo "Starting client $i"
+    python client.py &
+done
+
+# This will allow you to use CTRL+C to stop all background processes
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
+# Wait for all background processes to complete
+wait
+
+# Centralized Training
+python cifar.py
